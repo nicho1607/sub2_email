e309869e8e6bbe8c1d68807971fd95ea9c74fdad
nicho1607 <nicolacho1607@gmail.com>
2025-04-29 19:56:02 -0500

    sub2 terminado

--- config/email_config.py
+++ config/email_config.py
@@ -2,7 +2,7 @@ class EmailConfig:
     def __init__(self):
         self.smtp_server = "smtp.gmail.com"
         self.smtp_port = 587
-        self.email_from = "nicolacho1607@gmail.com"  # Cambiar por tu correo
-        self.email_password = "jcrg khuz jytf pfkr"   # Usar contraseña de aplicación
+        self.email_from = "nicolacho1607@gmail.com" 
+        self.email_password = "jcrg khuz jytf pfkr"   
         self.email_to = "Yilbertarboleda@gmail.com"
         self.email_subject = "Nueva Pre-Inscripción Recibida"
